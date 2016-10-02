9e36955913cf0f3ee7ef95d5a949f621e42b61b2
nikheizen <apricityacorn@gmail.com>
2016-10-01 22:25:44 -0700

    Create a game, with an actor and movement
    Currently only allows 4 directions of movement (left, right, up, down).
    The actor is currently just the '@' and it can leave the viewable
    screen.

---
+++ src/main.cc
@@ -0,0 +1,98 @@
+#include <ncurses.h>
+
+// Erase characters which would be left behind
+void erase(int row, int col)
+{
+	mvaddch(row, col, ' ');
+}
+
+void move_actor(int row, int col, char symbol)
+{
+	for(;;)
+	{
+		int ch = getch();
+
+		if (ch == 'h' || ch == 'H' || ch == KEY_LEFT || ch == '4')
+		{
+			erase(row, col);
+			col = col - 1;
+			mvaddch(row, col, symbol);
+			refresh();
+		}
+		if (ch == 'l' || ch == 'L' || ch == KEY_RIGHT || ch == '6')
+		{
+			erase(row, col);
+			col = col + 1;
+			mvaddch(row, col, symbol);
+			refresh();
+		}
+		if (ch == 'k' || ch == 'K' || ch == KEY_UP || ch == '8')
+		{
+			erase(row, col);
+			row = row - 1;
+			mvaddch(row, col, symbol);
+			refresh();
+		}
+		if (ch == 'j' || ch == 'J' || ch == KEY_DOWN || ch == '2')
+		{
+			erase(row, col);
+			row = row + 1;
+			mvaddch(row, col, symbol);
+			refresh();
+		}
+		else if(ch == 'q' || ch == 'Q')
+			break;
+	}
+}
+
+void game_loop(int ch)
+{
+	// Check if our user wants to play
+	if(ch == 'q' || ch =='Q') return;
+
+	// Slap an actor on the screen at (10,10)
+	int row = 10, col = 10;
+	char player_symbol = '@';
+
+	mvaddch(row, col, player_symbol);
+
+	// Allow movement!
+	move_actor(row, col, player_symbol);
+}
+
+void new_game()
+{
+	// Greet the user
+	printw("Welcome.\nPress the any key to play.\nPress [q] to quit!\n");
+
+	// Wait for user input then scrub
+	int ch = getch();
+	clear();
+
+	// Run the game loop
+	game_loop(ch);
+}
+
+void init_ncurses()
+{
+	initscr();
+	clear();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	curs_set(0);
+}
+
+int main()
+{
+	// Initialize the ncurses display
+	init_ncurses();
+
+	// Start a new game
+	new_game();
+
+	// Abolish ncurses
+	endwin();
+
+	return 0;
+}
